2b0f62946b9b2985e7f733366d4fd3efcc9c5eff
三浦圭亮 <k.miura.rainbow@gmail.com>
2018-05-14 09:16:03 +0900

    Teams master
    #WHAT
    作成し直し

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'prototypes#index'
+
+  resources :prototypes, only: [:index, :new, :create, :show, :destroy]
+  resources :users, only: [:show, :edit, :update, :destroy]
+end
